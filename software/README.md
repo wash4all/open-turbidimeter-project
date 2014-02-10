03a8ec2fe8488a189974fc36f3e5f6c43a867b63
Alex Krolick <whokilledtheelectricmonk@gmail.com>
2014-02-10 14:03:48 -0500

    Start adding SMS (text message) send/receive/parse functions.
    This commit breaks almost everything. Refer to the main branch ("master")
    for the latest stable code.

--- software/README.md
+++ software/README.md
@@ -1,32 +1,7 @@
-DO-IT-YOURSELF TURBIDIMETER
-===========================
-===========================
+# Software
 
-This setup will allow you to interact with a command-prompt turbidimeter over a serial connection.
+Contents:
 
-Required Hardware
-=================
+The folder called "turbidimeter" is the [Arduino](http://arduino.cc/en/main/software) project folder for the turbidimeter firmware.
 
-1 Arduino Uno microcontroller (or equivalent)
-2 TLS2561 luminosity sensor [http://www.adafruit.com/products/439]
-1 Electronics case [http://www.alliedelec.com/search/productdetail.aspx?SKU=70149027]
-1 Bluetooth slave module [http://www.amazon.com/BT2S-Bluetooth-to-Serial-Slave/dp/B006RBK9ZW?]
-1 5mm precision red LED [https://www.sparkfun.com/products/11393]
-  Jumper cables
-  Soldering iron
-
-Required Software
-==================
-Arduino IDE
-
-Additional information for Bluetooth
-==================
-
- Any device that can access a serial tty will work; we recommend using an Android phone with the Blueterm app (a tablet works with all features except data transmittal over SMS).
-
-Other
-===========
-
- To calibrate your turbidimeter to output NTUs (nephelometric turbidity standard units), you will either need to benchmark against a commercial model or purchase calibration materials from a vendor such as Procal. If that's not  an option, you'll have to make do with an arbitrary unit scale.
-
-----The Open Turbidimeter Project is a develop4wash initiative. Vist wash4all.org for more information. ------
+The "libraries" folder contains the libraries needed to compile the Arduino sketches. These need to be imported individually using "Sketch>Import Library" in the Arduino IDE _before_ complilation.
--- software/turbidimeter/turbidimeter.ino
+++ software/turbidimeter/turbidimeter.ino
@@ -1,14 +1,14 @@
 /*----------------OPEN TURBIDIMETER PROJECT---------------*/
-/*-------------------------ARDUINO CODE ----------------------*/
+/*--------------------- ARDUINO CODE ---------------------*/
 /*
-Developed by WASH4All, 2013. See wash4all.org for further
+Developed by WASH4All, 2014. See wash4all.org for further
 information about the project.
 */
 /*-------------------------LEGAL--------------------------*/
 
 /* This device and its components are licensed under a
-Creative Commons Attribution-NonCommercial-ShareAlike 3.0 
-Unported  (CC BY-NC-SA 3.0) licensed. In summary,
+Creative Commons Attribution-NonCommercial-ShareAlike 4.0 
+Unported  (CC BY-NC-SA 4.0) license. In summary,
 that means you are free to share (copy, distribute, and 
 transmit) and remix (adapt)the work, but you must adhere to 
 the following conditions:
@@ -30,210 +30,71 @@ If not, see: http://creativecommons.org/licenses/by-nc-sa/3.0
 
 #include <EEPROM.h>
 #include <Wire.h>
-#include "TSL2561.h"
 #include "EEPROMAnything.h"
 
-TSL2561 tsl1(TSL2561_ADDR_LOW); // the 90-deg unit's ADDR line is tied to ground
-TSL2561 tsl2(TSL2561_ADDR_FLOAT); // the 180-deg unit's ADDR line is floating
-int serialInput = 0; // variable holds most recent keystroke in main loop
-const int samples = 2; // number of samples required for calibration
-int samples_taken = 0; // how many calibration samples have been taken so far
-float turbidity;
-float full_90;
-float full_180;
-float percent_refraction;
-const int ledPin = 13;
-float NTU[2]; // for use in sampling routine, index starts at 0!
-float AU[2]; // for use in sampling routine, index starts at 0!
+/*ROLES:	each of which can have an associated sms password
+developer  -- for changing technical specs, checking in on usage stats, etc.
+admin	-- e.g. NGO that purchased the device
+user	-- POC for device
+group	-- group of users, e.g. water board or operators
+public	-- general public
+*/
 
-struct config_t // for storing data & retrieving data persistently
-{
-  float m;
-  float b;
-} 
-config;
+/*EXAMPLES:
+<group_pw>#adjsam#3600
+public#confon
+<group_pw>#passalert#your turbidimeter is sending weird readings
+<user_pw>#changeuser#<username> <useremail> <userphone>
+public#calibrate
+public#sendreading
+*/
 
-void setup(void) {
-  EEPROM_readAnything(0, config);
+void setup(){
   Serial.begin(9600);
-  pinMode(13, OUTPUT); 
-  delay(1000);
-  if (tsl1.begin()) { // this is always true for some reason
-    Serial.println("Found sensor");
-  } 
-  else {
-    Serial.println("No sensor?");
-    while (1){};
-  }
-
-  if (Serial.available() >= 0) { // should return -1 if no serial available
-    Serial.println("Waiting for input. Press r to read, c to calibrate.");
-  }
-  // configure TLS2651 luminosity sensor:
-  // You can change the gain on the fly, to adapt to brighter/dimmer light situations
-  //tsl1.setGain(TSL2561_GAIN_0X);         // set no gain (for bright situtations)
-  tsl1.setGain(TSL2561_GAIN_16X);      // set 16x gain (for dim situations)
-  tsl2.setGain(TSL2561_GAIN_16X);      // set 16x gain (for dim situations)
-  // Changing the integration time gives you a longer time over which to sense light
-  // longer timelines are slower, but are good in very low light situtations!
-  //tsl1.setTiming(TSL2561_INTEGRATIONTIME_13MS);  // shortest integration time (bright light)
-  tsl1.setTiming(TSL2561_INTEGRATIONTIME_402MS);  // medium integration time (medium light)
-  tsl2.setTiming(TSL2561_INTEGRATIONTIME_101MS);  // medium integration time (medium light)
-  //tsl1.setTiming(TSL2561_INTEGRATIONTIME_402MS);  // longest integration time (dim light)
+  //Set up modem functions
+  //put modem to sleep
 }
 
-void loop() {
-  if (Serial.available() > 0) { // if there's a serial connection, get commands from it
-    serialInput = Serial.read(); // get one-byte command option
-    Serial.println((char)serialInput); // tell the user what they pressed
+void loop(){
+  //turn on modem
+  String s = getMessageText(); //say output is "<password>#adjsam#3600"
+  parseMessage(s);
+  //turn off modem
+  delay(900000);
+}
 
-    switch (serialInput) { // do an action based on keypress
-    case 'r': // read   
-      Serial.println("Taking readings...");
-      take_readings(20, 100);
-      break;
-    case 'c': // calibrate    
-      calibrate();
-      break;
-    case 'u': // cancels calibration in progress
-      samples_taken = 0;
-      break;
-    case 's': // saves calibration to persistent memory
-      EEPROM_writeAnything(0, config);
-      Serial.println("Config saved. Press r to read, c to recalibrate.");
-      break;
-    default: // if unknown key was pressed
-      Serial.println("Invalid input. Press r to read, c to calibrate.");
+void parseMessage(String s){
+  String s1, s2, s3;
+  int i1 = s.indexOf("#");
+  int i2 = s.lastIndexOf("#");
+  s1 = s.substring(0,i1);
+  s2 = s.substring(i1+1,i2);
+  s3 = s.substring(i2+1,s.length());
+  s = "";
+  if(s1 == "<password>"){
+    Serial.println("passes");
+    if(s2 == "adjsam"){
+      Serial.println("adjust sampling rate to " + s3 + " seconds per sample...");
+    }else if(s2 == "confon"){
+      Serial.println("send a message confirming receipt of message...");
+      //send message to <remote_num> with text "<sensor_id> running"
+    }else if(s2 == "passalert"){
+      Serial.println("pass the contents of this message on to the ...");
+      //send message to <user_num> with text (words[i] for i > 2)
+    }else if(s2 == "changeuser"){
+      Serial.println("change the stored user name and number...");
+      //delete old user struct
+      //EEPROM a struct with {'userfn': <POC_first_name>, 'useremail':<POC_email>, 'userpn': <POC_phone_num>}
+    }else if(s2 == "calibdate"){
+      Serial.println("send a message with the date of the last device calibration...");
+      //retrieve <last_calibration_date> and send sms to <remote_num>
+    }else if(s2 == "sendrdg"){
+      Serial.println("send an immediate turbidity reading...");
+      //turn_on_sensor and take_reading
+      //send message <remote_num> with <sensor_reading>
+    }else{
+      Serial.println("command unrecognized");
+      //do nothing
     }
-
-  } 
-}
-
-void take_readings(int reps, int spacing){
-  float dark_90 = 0.0; 
-  float dark_180 =0.0;
-  float far1 = 0.0;  
-  float far2 = 0.0;
-  float full_90 = 0.0;
-  float full_180 = 0.0;
-  uint16_t lum = 0.0;
-  
-  digitalWrite(ledPin, LOW); //measure ambient light level
-  lum = tsl1.getLuminosity(TSL2561_FULLSPECTRUM);
-    dark_90 = lum;
-  lum = tsl2.getLuminosity(TSL2561_FULLSPECTRUM);
-    dark_180 = lum;
-    
-  digitalWrite(ledPin, HIGH); //turn on light & read sample
-  for(int i = 0; i < reps; i++){
-    lum = tsl1.getLuminosity(TSL2561_FULLSPECTRUM);
-    far1 += lum;
-    lum = tsl2.getLuminosity(TSL2561_FULLSPECTRUM);
-    far2 += lum;
-    //delay(spacing);
-  }
-  digitalWrite(ledPin, LOW); //turn off LED
-  full_90 = (far1-dark_90)/(float)reps; //subtract ambient
-  full_180 = (far2-dark_180)/(float)reps; //and divide by # samples
-  Serial.print("90-deg: ");  
-  Serial.println(full_90);
-  Serial.print("180-deg: "); 
-  Serial.println(full_180);
-  turbidity = (full_90 - config.b)/config.m; //calculate from x=(y-b)/m
-  Serial.print("Turbidity: "); 
-  Serial.print(turbidity); 
-  Serial.println(" NTU");
-}
-
-void calibrate(){
-  float dark_90 = 0.0; 
-  float dark_180 =0.0;
-  float far1 = 0.0;  
-  float far2 = 0.0;
-  float full_90 = 0.0;
-  float full_180 = 0.0;
-  uint16_t lum = 0.0;
-  const int readings = 20; // how many readings to take from sensor
-
-  digitalWrite(ledPin, LOW);
-  dark_90 = tsl1.getLuminosity(TSL2561_FULLSPECTRUM); 
-  dark_180 = tsl2.getLuminosity(TSL2561_FULLSPECTRUM); 
-
-  if (samples_taken < samples){
-    samples_taken = samples_taken + 1;
-    Serial.println("Loading calibration routine...");
-    Serial.println("Enter the NTU for this sample (5s...)");
-    delay(5000); // wait for user
-    float sample_NTU = Serial.parseFloat(); // works with Arduino serial monitor
-    //float sample_NTU = readFloatFromBytes(); // works with Python (see function comments)
-    Serial.println(sample_NTU, DEC); // print recorded value
-    Serial.println("Reading sensor");
-    digitalWrite(ledPin, HIGH);
-    
-    for(int i = 0; i < readings; i++){ // take #readings amount of samples
-      far1 += tsl1.getLuminosity(TSL2561_FULLSPECTRUM);  // read from sensor 1
-      far2 += tsl2.getLuminosity(TSL2561_FULLSPECTRUM);  // read from sensor 2
-    }
-    
-    digitalWrite(ledPin, LOW); //turn off LED
-    full_90 = (far1 - dark_90)/(float)readings; // subtract ambient light & ...
-    full_180 = (far2- dark_180)/(float)readings; // divide by number of sampling intervals
-
-    Serial.print("90-deg: ");  
-    Serial.println(full_90);
-    Serial.print("180-deg: "); 
-    Serial.println(full_180);
-    
-    NTU[samples_taken-1] = sample_NTU; //NTU for sample
-    AU[samples_taken-1] = full_90; //Arbitrary Units for sample (what sensor returns)
-    
-    int percent = samples_taken  * 100 / samples;
-    Serial.print(percent); 
-    Serial.print("% complete. ");
-    Serial.println("Press c to continue, or u to cancel...");
-  }
-  else { //calculate slope & intercept of line between pts (y = mx+b)
-    float delta_y = AU[1] - AU[0];
-    float delta_x = NTU[1] - NTU[0];
-    float m = delta_y / delta_x; //slope
-    float b = AU[0] - (m * NTU[0]); //intercept
-    config.m = m;
-    config.b = b;
-    samples_taken = 0; //reset counter
-    Serial.println("Calibration complete. y=mx+b (NTU = x, AU = y):");
-    Serial.print("m: ");
-    Serial.println(config.m, DEC);
-    Serial.print("b: ");
-    Serial.println(config.b, DEC);
-    Serial.println("Press s to save calibration for next time."); 
   }
 }
-
-float readFloatFromBytes() {
-  /* converts 4-byte float sent from python via serial port to Arduino float
-   * python commands to send data:
-   * > import struct
-   * > import serial
-   * > ser = serial.Serial('/dev/ttyACM0 or whatever')
-   * > ser.write(struct.pack('f', YOURVALUE) 
-   * or send the calibrate command and value at the same time:
-   * > ser.write('c' + struct.pack('f', YOURVALUE)
-   */
-  union u_tag {
-    byte b[4];
-    float fval;
-  } 
-  u;
-  u.b[0] = Serial.read();
-  u.b[1] = Serial.read();
-  u.b[2] = Serial.read();
-  u.b[3] = Serial.read();
-  return u.fval; // returns the float
-}
-
-
-
-
-
-
-
